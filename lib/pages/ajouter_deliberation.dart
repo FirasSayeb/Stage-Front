5eab4a066e2253423539d4dc0083ead8a078d233
FirasSayeb <firassayeb2@gmail.com>
2024-03-15 12:09:29 +0100

    add notification

--- lib/pages/ajouter_deliberation.dart
+++ lib/pages/ajouter_deliberation.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:app/pages/Admin.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:file_picker/file_picker.dart';
+import 'package:http/http.dart';
 import 'package:lottie/lottie.dart';
 
 class AjouterDel extends StatefulWidget {
@@ -17,6 +19,7 @@ class AjouterDel extends StatefulWidget {
 class _AjouterDelState extends State<AjouterDel> {
   final GlobalKey<FormState> fkey = GlobalKey<FormState>();
   PlatformFile? file;
+  late Response response2;
   String errorMessage = '';
 
   Future<void> pickSingleFile() async {
@@ -46,12 +49,13 @@ class _AjouterDelState extends State<AjouterDel> {
       'POST',
       Uri.parse("http://10.0.2.2:8000/api/addNote"),
     ); 
-    request.files.add(http.MultipartFile.fromBytes(
+    request.fields['email'] = widget.email;
+    request.files.add(http.MultipartFile.fromBytes( 
       'file',    
       fileBytes,
       filename: file!.name,              
     ));
-     
+      
      
     var response = await request.send();
     print(fileBytes);
@@ -106,22 +110,48 @@ class _AjouterDelState extends State<AjouterDel> {
                 ),
               ),
               Center(
-                child: GestureDetector(
-                  onTap: () async {
-                    if (fkey.currentState!.validate()) {
-                      fkey.currentState!.save();
-                      await uploadFile();
-                    }
+                child: FutureBuilder(
+                  future: getParents(),
+                  builder:(context, snapshot) {
+                    return GestureDetector(
+                    onTap: () async {
+                      if (fkey.currentState!.validate()) {
+                        fkey.currentState!.save();
+                        await uploadFile(); 
+                        for(int i=0;i<snapshot.data!.length;i++){
+                            response2 = await post(
+                  Uri.parse('https://fcm.googleapis.com/fcm/send'),
+                  headers: {    
+                    'Content-Type': 'application/json',
+                    'Authorization':
+                        'key=AAAA4WMATYA:APA91bFxzOAlkcvXkHv6pyk9-Bqb8rtUwF6TXiBiEAQLuiGUwr6X084p-GR2lSSfJM_-H6urIktOdKGYhqPjKEscHN9XoxN8AMMvxXjbq27ZzQbk-S589EH-euzjPeduKyoXgt1lXuSE',
                   },
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
-                    margin: const EdgeInsets.symmetric(horizontal: 20),
-                    decoration: BoxDecoration(
-                      color: Colors.lightBlueAccent,
-                      borderRadius: BorderRadius.circular(8),
+                  body: jsonEncode({
+                    "to": snapshot.data![i]['token'],
+                    "notification": {"title": "Notification", "body": "Notes Sont Disponibles"}  
+                  }),  
+                ); 
+                print("-------------------------------");
+                print( { 
+                    "to": snapshot.data![i]['token'],
+                    "notification": {"title": "Notification", "body": "Notes Sont Disponibles"}
+                  }
+                    );
+                        }
+                      }
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlueAccent,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: const Text("Ajouter "),
                     ),
-                    child: const Text("Ajouter "),
-                  ),
+                  );
+                  },
+                   
                 ),
               ),
               Padding(padding: EdgeInsets.all(5)),
@@ -148,5 +178,20 @@ class _AjouterDelState extends State<AjouterDel> {
         ),
       ),
     );
+  }
+  Future<List<Map<String,dynamic>>> getParents()async { 
+   try{ 
+   final response =await get(Uri.parse("http://10.0.2.2:8000/api/getParents"));
+   if(response.statusCode==200){
+     List<dynamic> classesData = jsonDecode(response.body)['list'];
+      List<Map<String, dynamic>> classes = List<Map<String, dynamic>>.from(classesData);
+      return classes;
+   }else{
+    throw Exception('failed to get parents');
+   } 
+   }catch(e){ 
+    print(e); 
+    throw Exception('Failed to load parents'); 
+   }
   }
 }
