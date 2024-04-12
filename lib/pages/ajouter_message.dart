72aea4f8dcd3bac4031f44f30afbe729c275c266
FirasSayeb <firassayeb2@gmail.com>
2024-04-12 11:33:38 +0100

    fix list users

--- lib/pages/ajouter_message.dart
+++ lib/pages/ajouter_message.dart
@@ -186,20 +186,23 @@ class _AjouterMessageState extends State<AjouterMessage> {
     ),
     ); 
   }
-  Future<List<Map<String,dynamic>>> getUsers()async { 
-   try{ 
-   final response =await get(Uri.parse("https://firas.alwaysdata.net/api/getUserss/${widget.email}"));
-   if(response.statusCode==200){
-     List<dynamic> classesData = jsonDecode(response.body)['list'];
-      List<Map<String, dynamic>> classes = List<Map<String, dynamic>>.from(classesData);
-      return classes;
-   }else{
-    throw Exception('failed to get users');
-   }
-   }catch(e){
-    print(e); 
-    throw Exception('Failed to load users'); 
-   }
+  Future<List<Map<String, dynamic>>> getUsers() async {
+  try {
+    final response = await get(Uri.parse("https://firas.alwaysdata.net/api/getUserss/${widget.email}"));
+    if (response.statusCode == 200) {
+      // Parse the response JSON object into a list of maps
+      Map<String, dynamic> responseData = jsonDecode(response.body)['list'];
+      List<Map<String, dynamic>> users = responseData.values.toList().cast<Map<String, dynamic>>();
+      return users;
+    } else {
+      throw Exception('Failed to get users');
+    }
+  } catch (e) {
+    print(e);
+    throw Exception('Failed to load users');
   }
+}
+
+
 
 }
