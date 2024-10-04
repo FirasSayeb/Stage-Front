80c5e25de4f1ccc28a7ec36d709e9ade33e47535
FirasSayeb <firassayeb2@gmail.com>
2024-10-04 08:42:11 +0100

    fixing sending notification

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
  // Platform.isAndroid ?        
   await Firebase.initializeApp( 
-    options: const FirebaseOptions(apiKey:"AIzaSyAusdpI9iXcm4N5mqiZcBysfJTYws_wMaI" , appId:"1:968028605824:android:5cdd68fdb5be3a9b3a0565" , messagingSenderId:"968028605824" , projectId:"flutterapp-91de5" )
+    options: const FirebaseOptions(apiKey:"" , appId:"" , messagingSenderId:"" , projectId:"" )
   );  
   //:await Firebase.initializeApp(); 
  // await NotificationService().initNotification();    
   runApp(const MaterialApp(   
     debugShowCheckedModeBanner: false,    
     home: Home(),        
-  )); 
+  ));  
 }
    
